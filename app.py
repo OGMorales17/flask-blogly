69c149ba9ad2821cb8b46a12b5f2ed995c3f7101
Orlando G Morales <orlandomorales@Orlandos-MacBook-Pro.local>
2021-06-14 14:20:25 -0400

    still working on part two

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_sqlalchemy import SQLAlchemy
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 
@@ -94,3 +94,83 @@ def users_destroy(user_id):
     db.session.commit()
 
     return redirect("/users")
+
+
+##############  Part two  ###################
+
+@app.route('/posts/')
+def post_root():
+    """Show the most recent post"""
+
+    posts = Post.query.order_by(Post.created_at.desc()).limit(5).all()
+    return render_template("posts/homepage.html", posts=posts)
+
+
+@app.route('/users/<int:user_id>/posts/new')
+def posts_new_form(user_id):
+    """Show a form to create a new post for the current user"""
+
+    user = User.query.get_or_404(user_id)
+    return render_template('posts/new.html', user=user)
+
+
+@app.route('/users/<int:user_id>/posts/new', methods=["POST"])
+def posts_new(user_id):
+    """Handle form submission for creating a new post"""
+
+    user = User.query.get_or_404(user_id)
+    new_post = Post(
+        title=request.form['title'],
+        content=request.form['content'],
+        user=user)
+
+    db.session.add(new_post)
+    db.session.commit()
+    flash(f"Posts '{new_post.title}' added.")
+
+    return redirect(f"/users/{user_id}")
+
+
+@app.route('/posts/<int:post_id>')
+def posts_show(post_id):
+    """Show a page with info on a specific post"""
+
+    post = Post.query.get_or_404(post_id)
+    return render_template('posts/detail.html', post=post)
+
+
+@app.route('/posts/<int:post_id>/edit')
+def posts_edit(post_id):
+    """Show a form to edit an existing post"""
+
+    post = Post.query.get_or_404(post_id)
+    return render_template('posts/edit.html', post=post)
+
+
+@app.route('/posts/<int:post_id>/edit', methods=["POST"])
+def posts_update(post_id):
+    """Handle form submission for updating an existing post"""
+
+    post = Post.query.get_or_404(post_id)
+    post.title = request.form['title']
+    post.content = request.form['content']
+
+    db.session.add(post)
+    db.session.commit()
+    flash(f"Post '{post.title}' edited")
+
+    return redirect(f"/users/{post.user.id}")
+
+
+@app.route('/posts/<int:post_id>/delete', methods=["POST"])
+def posts_destroy(post_id):
+    """Handle form submission for deleting an existing post"""
+
+    post = Post.query.get_or_404(post_id)
+
+    db.session.delete(post)
+    db.session.commit()
+
+    flash(f"Post '{post.title}' deleted.")
+
+    return redirect(f"/users")
--- models.py
+++ models.py
@@ -1,5 +1,10 @@
 """Models for Blogly."""
+import datetime
+from threading import current_thread
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import backref
+from enum import unique
+
 
 db = SQLAlchemy()
 
@@ -24,13 +29,64 @@ class User(db.Model):
 
     image_url = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE_URL)
 
+    posts = db.relationship('Post', backref='user',
+                            cascade='all, delete-orphan')
+
     @property
     def full_name(self):
         """Return full name of user"""
         return f"{self.first_name} {self.last_name}"
 
 
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    post_id = db.Column(db.Integer,
+                        primary_key=True,
+                        autoincrement=True)
+
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+
+    created_at = db.Column(db.DateTime(timezone=True),
+                           nullable=False, default=datetime.datetime.now())
+
+    id = db.Column(db.Integer, db.ForeignKey(
+        'users.id'), nullable=False)
+
+    @property
+    def date_formatted(self):
+        """Return created time formatted"""
+        return self.created_at.strftime(" Created on: %a %b %-d %Y, %-I:%M %p")
+
+
 def connect_db(app):
 
     db.app = app
     db.init_app(app)
+
+# Down this line is what I had to modified
+
+    # @property
+    # def date_formatted(self):
+    #     """Return created time formatted"""
+    #     return "<Example(id=%s, created_at=%s)>" % (self.id, self.created_at)
+
+
+# class Post(db.Model):
+#     __tablename__ = 'posts'
+
+#     post_id = db.Column(db.Integer,
+#                         primary_key=True,
+#                         autoincrement=True)
+
+#     title = db.Column(db.Text, nullable=False)
+#     content = db.Column(db.Text, nullable=False)
+
+#     created_at = db.Column(db.DateTime(timezone=True),
+#                            nullable=False, default=datetime.datetime.now())
+
+#     id = db.Column(db.Integer, db.ForeignKey(
+#         'users.id'), nullable=False)
+
+#     user = db.relationship('User', backref='posts')
